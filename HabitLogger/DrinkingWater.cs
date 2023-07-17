42e2ab549fb157201a8a5ed145a561655ab45878
Nathan Quan <nathansquan@gmail.com>
2023-07-17 18:31:42 -0400

    Add create, read, delete functionality

---
+++ HabitLogger/DrinkingWater.cs
@@ -0,0 +1,8 @@
+namespace HabitLogger;
+
+public class DrinkingWater
+{
+    public int Id { get; set; }
+    public DateTime Date { get; set; }
+    public int Quantity { get; set; }
+}
--- HabitLogger/Program.cs
+++ HabitLogger/Program.cs
@@ -1,19 +1,4 @@
-﻿using Microsoft.Data.Sqlite;
+﻿
+using HabitLogger;
 
-string connectionString = @"Data Source=habitTracker.db";
-
-using (var connection = new SqliteConnection(connectionString))
-{
-    connection.Open();
-    var command= connection.CreateCommand();
-
-    command.CommandText = 
-        @"CREATE TABLE IF NOT EXISTS drinking_water (
-            Id INTEGER PRIMARY KEY AUTOINCREMENT,
-            Date TEXT,
-            Quantity INTEGER)";
-    
-    command.ExecuteNonQuery();
-    
-    connection.Close();
-}
+Crud.GetUserInput();
